3d70120bd83031fea15429dd209942c246135373
Baubs <pathogenic@gmail.com>
2022-04-21 21:39:33 -0500

    start of project

---
+++ src/main/java/com/qrcraze/qrcraze/QrcrazeApplication.java
@@ -0,0 +1,17 @@
+package com.qrcraze.qrcraze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class QrcrazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcrazeApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/qrcraze/qrcraze/QrcrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcraze.qrcraze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcrazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
